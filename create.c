07c95ad522c4ab83954f03a4a920bdfc0ffaf21e
guardiansofgardengrove <48894056+guardiansofgardengrove@users.noreply.github.com>
2019-03-24 16:25:43 -0700

    Add files via upload
    first entry of the create code

---
+++ create.c
@@ -0,0 +1,28 @@
+#include <kipr/botball.h>
+
+int main()
+{
+    printf("This is the create's code\n");
+    
+    //Intial functions (wait_for_light and shut_down_in)
+    shut_down_in(119); //stop all code in 120 seconds
+    //wait_for_light(3); [added at competition]
+    create_connect(); //connect wallaby to create
+    
+    //List integers:
+    
+    //Set the servos to the default postions (Do not forget to do this at the competetion!!!)
+    
+    
+    //Official start of code:
+    //Quickly back up for alignment
+    
+    //Turn right to head towards the power lines
+    
+    //Drive straight until it senses the r
+
+    create_disconnect();
+    
+    //function for people sweeping
+    return 0;
+}

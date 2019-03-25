ca4d08eef45115030573d34b5c8dcc3edccc7121
guardiansofgardengrove <48894056+guardiansofgardengrove@users.noreply.github.com>
2019-03-24 18:30:50 -0700

    Update create.c
    Added the safe arm startup code.

--- create.c
+++ create.c
@@ -5,16 +5,22 @@ int main()
     printf("This is the create's code\n");
     
     //Intial functions (wait_for_light and shut_down_in)
+    enable_servos();
     shut_down_in(119); //stop all code in 120 seconds
-    //wait_for_light(3); [added at competition]
     create_connect(); //connect wallaby to create
-    
     //List integers:
-    
-    //Set the servos to the default postions (Do not forget to do this at the competetion!!!)
-    
+    int power = 3;
+    int people = 0;
+    int powerP = get_servo_position(power);
+    int peopleP = get_servo_position(people);
     
     //Official start of code:
+    //unfold the robot:
+        while (get_servo_position(power) < 1550) {
+        powerP = powerP+25;
+        msleep(15);
+        set_servo_position(power,powerP);
+    }   
     //Quickly back up for alignment
     
     //Turn right to head towards the power lines

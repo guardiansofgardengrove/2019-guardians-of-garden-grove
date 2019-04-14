f3a3d473a3011d35f7df56ebc014684402e12110
guardiansofgardengrove <48894056+guardiansofgardengrove@users.noreply.github.com>
2019-04-13 20:59:52 -0700

    Update create.c
    refined electrical lines (in progress)

--- create.c
+++ create.c
@@ -14,32 +14,34 @@ int main()
     //startup prerun functions
     create_connect(); //connect wallaby to create
     msleep(100);
-    printf("Create Batter Power Before: %d",get_create_battery_charge());
+    printf("Create Batter Power Before run: %d",get_create_battery_charge());
     printf("/n");
-    msleep(3000);
+    msleep(1000);
     enable_servo(power);
-        while (get_servo_position(power) < 1400) {
-        powerP = powerP+25;
+        /*while (get_servo_position(power) < 1400) {
+        powerP = powerP+75;
         msleep(100);
         set_servo_position(power,powerP);
-    } 
-    enable_servo(people);
+    } */
     msleep(1000);
-        while (get_servo_position(people) < 2020) {
-        peopleP = peopleP+25;
+    enable_servo(people);
+        /*while (get_servo_position(people) < 2020) {
+        peopleP = peopleP+75;
         msleep(100);
         set_servo_position(people,peopleP);    
-    }
+    }*/
     set_servo_position(people,2040);    
-        while (get_servo_position(power) > 600) {
+        /*while (get_servo_position(power) > 600) {
         powerP = powerP-25;
         msleep(100);
         set_servo_position(power,powerP);
-    } 
+    } */
     set_servo_position(power,600);
      
     msleep(500);//wait for light
     shut_down_in(119); //stop all code in 120 seconds
+    printf("Create Batter Power at start: %d",get_create_battery_charge());
+    printf("/n");
     
     //Official start of code:
     //unfold the robot:
@@ -71,113 +73,36 @@ int main()
     create_spin_CCW(200);
     msleep(960);
     create_drive_direct(200,200);
-    msleep(1250);
+    msleep(1250); //1250 1500
     create_spin_CW(200);
-    msleep(960);
+    msleep(930);
     
-    //aproach diagonal turn
-    /*create_drive_direct(200,-200);
-    msleep(quarter);
-    create_stop();
-    
-    create_drive_direct(200,200);
-    msleep(3000);
-    create_stop();
-    
-    create_drive_direct(-200,200);
-    msleep(quarter);
-    create_stop();
-    
-    create_drive_direct(-100,-100);
-    msleep(3000);
-    create_stop();
-    
-    //complet diagonal 3 part turn to get to power lines
-    create_drive_direct(200,200);
-    msleep(1500);
-    create_stop();
-    
-    create_drive_direct(200,-200);
-    msleep(quarter/2);
-    create_stop();
-    
-    create_drive_direct(200,200);
-    msleep(1500);
-    create_stop();
-    
-    create_drive_direct(200,-200);
-    msleep(quarter/2 +100);
-    create_stop();*/
     //slowly approach the power lines
     while (get_create_rbump() == 0 || get_create_lbump() == 0) {
         create_drive_direct(200,200); //100
     }
     create_stop();
     
-    /*create_drive_direct(-100,-100);
-    msleep(250);
-    create_stop();
-    
-    create_drive_direct(-200,200);
-    msleep(quarter);//special case for slight allingment issue
-    create_stop();
-    
-    create_drive_direct(-200,-200);
-    msleep(1900);
-    create_stop();
-    
-    create_drive_direct(200,200);
-    msleep(500);
-    create_stop();
-    
-    //unfold the robot: to get the first washer
-        while (get_servo_position(power) < 1850) {
-        powerP = powerP+25;
-        msleep(25);
-        set_servo_position(power,powerP);
-    }
-    
-    //position itself for washer
-    create_drive_direct(100,100);
-    msleep(1500);
-    create_stop();
-    msleep(500);
-    
-       // while (get_servo_position(power) > 1550) {
-        //powerP = powerP+25;
-        //msleep(25);
-        //set_servo_position(power,powerP);
-    //}
-    msleep(500);
-    
-    //score the first point
-    create_drive_direct(100,100);
-    msleep(3000);
-    create_stop();
-    
-   	msleep(20); 
-    create_disconnect();
-    msleep(20);*/
-    
     //position for right washer
     create_spin_CCW(200);
-    msleep(1000);//960
+    msleep(940);//960 1000 960
     create_drive_direct(-100,-100);
-    msleep(750);
-    set_servo_position(power, 1520);
-    msleep(1000); //added
+    msleep(1150); //750 1000 //1250 1150
+    set_servo_position(power, 1620); //1520
+    //msleep(1000); //added
     
     //connect washer and push container
     create_drive_direct(-150,-150);
-    msleep(1200);
-    while (get_digital_value(8) == 0 && get_digital_value(9) == 0) {
-    create_drive_direct(-35,-35); //200
+    msleep(1500); //1200 1500 1675
+    /*while (get_digital_value(8) == 0 && get_digital_value(9) == 0) {
+    create_drive_direct(-100,-100); //200
     //msleep(1200);
-    }
+    }*/
     create_stop();
+    msleep(1000);
     
     //unhook and turn for next washer
-    set_servo_position(power, 1700);
+    set_servo_position(power, 1800); //1700
     create_spin_CCW(200);
     msleep(480);
     
@@ -192,14 +117,14 @@ int main()
     msleep(500);
     
     //connect next washer
-    set_servo_position(power, 1575);
+    set_servo_position(power, 1625); //1575
     msleep(1000); //added
     create_drive_direct(150,150);
-    msleep(1350); //1350
-    while (get_digital_value(8) == 0 && get_digital_value(9) == 0) {  
+    msleep(1600); //1350
+    /*while (get_digital_value(8) == 0 && get_digital_value(9) == 0) {  
     create_drive_direct(35,35); //200 //75
     }
-    create_stop(); //added
+    create_stop(); //added */
     
     //disconnect from washer
     set_servo_position(power, 1800);
@@ -212,7 +137,12 @@ int main()
     //back up and sweep EVERYTHING
     create_spin_CCW(200);
     msleep(480);
-    create_drive_direct(-400,-400);
+    
+    while (analog(0) < 3000){
+        create_drive_direct(-200,-200);
+    }
+    create_stop();
+    /*create_drive_direct(-400,-400);
     msleep(1000);
     create_spin_CW(200);
     msleep(950);
@@ -221,6 +151,7 @@ int main()
     create_stop();
     msleep(1000);
         printf("Create Batter Power after: %d",get_create_battery_charge());
+        */
     
     return 0;
 }
